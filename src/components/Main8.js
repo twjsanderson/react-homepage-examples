cc02cbffe22ae6b8039f7bf27dccd62a0f9cc880
vagrant <tom.w.j.sanderson@gmail.com>
2019-10-08 03:04:14 +0000

    new main8

---
+++ src/components/Main8.js
@@ -0,0 +1,69 @@
+import React, { Component } from 'react';
+import { Row, Col, Container, Card } from 'react-bootstrap';
+
+// TODO: import font awesome and use below, add header above grid with background
+
+class Main8 extends Component {
+  render() {
+    return (
+        <section id="main8">
+            <Container>
+                <Row className="mb-2">
+                    <Col sm={4}>
+                        <Card className="bg-danger text-white text-center">
+                            <Card.Body>
+                                <h3>Stuff Guy</h3>
+                                <p>ok go for it bud pall thanks.</p>
+                            </Card.Body>
+                        </Card>
+                    </Col>
+                    <Col sm={4}>
+                        <Card className="bg-danger text-white text-center">
+                            <Card.Body>
+                                <h3>Stuff Guy</h3>
+                                <p>ok go for it bud pall thanks.</p>
+                            </Card.Body>
+                        </Card>
+                    </Col>
+                    <Col sm={4}>
+                        <Card className="bg-danger text-white text-center">
+                            <Card.Body>
+                                <h3>Stuff Guy</h3>
+                                <p>ok go for it bud pall thanks.</p>
+                            </Card.Body>
+                        </Card>
+                    </Col>
+                </Row>
+                <Row className="mb-2">
+                    <Col sm={4}>
+                        <Card className="bg-danger text-white text-center">
+                            <Card.Body>
+                                <h3>Stuff Guy</h3>
+                                <p>ok go for it bud pall thanks.</p>
+                            </Card.Body>
+                        </Card>
+                    </Col>
+                    <Col sm={4}>
+                        <Card className="bg-danger text-white text-center">
+                            <Card.Body>
+                                <h3>Stuff Guy</h3>
+                                <p>ok go for it bud pall thanks.</p>
+                            </Card.Body>
+                        </Card>
+                    </Col>
+                    <Col sm={4}>
+                        <Card className="bg-danger text-white text-center">
+                            <Card.Body>
+                                <h3>Stuff Guy</h3>
+                                <p>ok go for it bud pall thanks.</p>
+                            </Card.Body>
+                        </Card>
+                    </Col>
+                </Row>
+            </Container>
+        </section>
+    );
+  }
+}
+
+export default Main8;
--- src/index.js
+++ src/index.js
@@ -11,7 +11,7 @@ import Main4 from './components/Main4';
 import Main5 from './components/Main5'; 
 import Main6 from './components/Main6';
 import Main7 from './components/Main7';
-// import Main8 from './components/Main8';
+import Main8 from './components/Main8';
 // import Main9 from './components/Main9';
 // import Main10 from './components/Main10';
 import './index.css';
@@ -29,8 +29,8 @@ ReactDOM.render(
                 <Link className="p-3" to="/main5">Main5</Link>
                 <Link className="p-3" to="/main6">Main6</Link>
                 <Link className="p-3" to="/main7">Main7</Link>
-                {/* <Link className="link" to="/main8">Main8</Link>
-                <Link className="link" to="/main9">Main9</Link>
+                <Link className="link" to="/main8">Main8</Link>
+                {/* <Link className="link" to="/main9">Main9</Link>
                 <Link className="link" to="/main10">Main10</Link> */}
             </Col>
         </Row>
@@ -42,8 +42,8 @@ ReactDOM.render(
             <Route path="/main5" component={Main5} />
             <Route path="/main6" component={Main6} />
             <Route path="/main7" component={Main7} />
-            {/* <Route path="/main8" component={Main8} />
-            <Route path="/main9" component={Main9} />
+            <Route path="/main8" component={Main8} />
+            {/* <Route path="/main9" component={Main9} />
             <Route path="/main10" component={Main10} />     */}
         </Switch>
         </Container>
