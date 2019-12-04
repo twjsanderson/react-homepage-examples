c7571bb5914436eda44fa8f859f6bd2779c19c5a
vagrant <tom.w.j.sanderson@gmail.com>
2019-12-04 17:09:51 +0000

    finished all main mock ups

--- src/components/Main8.js
+++ src/components/Main8.js
@@ -1,69 +1,31 @@
-import React, { Component } from 'react';
-import { Row, Col, Container, Card } from 'react-bootstrap';
+import React, { Fragment } from 'react';
+import Nav1 from './NavBar';
+import { Container, Row, Col } from 'react-bootstrap';
+import video from '../assets/video.mp4';
 
-// TODO: import font awesome and use below, add header above grid with background
+// requires videoContainer class with position: relative, and overlay with position absolute and a few other alignments
 
-class Main8 extends Component {
-  render() {
-    return (
-        <section id="main8">
-            <Container>
-                <Row className="mb-2">
-                    <Col sm={4}>
-                        <Card className="bg-danger text-white text-center">
-                            <Card.Body>
-                                <h3>Stuff Guy</h3>
-                                <p>ok go for it bud pall thanks.</p>
-                            </Card.Body>
-                        </Card>
-                    </Col>
-                    <Col sm={4}>
-                        <Card className="bg-danger text-white text-center">
-                            <Card.Body>
-                                <h3>Stuff Guy</h3>
-                                <p>ok go for it bud pall thanks.</p>
-                            </Card.Body>
-                        </Card>
-                    </Col>
-                    <Col sm={4}>
-                        <Card className="bg-danger text-white text-center">
-                            <Card.Body>
-                                <h3>Stuff Guy</h3>
-                                <p>ok go for it bud pall thanks.</p>
-                            </Card.Body>
-                        </Card>
-                    </Col>
-                </Row>
-                <Row className="mb-2">
-                    <Col sm={4}>
-                        <Card className="bg-danger text-white text-center">
-                            <Card.Body>
-                                <h3>Stuff Guy</h3>
-                                <p>ok go for it bud pall thanks.</p>
-                            </Card.Body>
-                        </Card>
-                    </Col>
-                    <Col sm={4}>
-                        <Card className="bg-danger text-white text-center">
-                            <Card.Body>
-                                <h3>Stuff Guy</h3>
-                                <p>ok go for it bud pall thanks.</p>
-                            </Card.Body>
-                        </Card>
-                    </Col>
-                    <Col sm={4}>
-                        <Card className="bg-danger text-white text-center">
-                            <Card.Body>
-                                <h3>Stuff Guy</h3>
-                                <p>ok go for it bud pall thanks.</p>
-                            </Card.Body>
-                        </Card>
+const Main8 = () => {
+  return (
+    <Fragment>
+        <Nav1 />
+        <Container fluid className="videoContainer" style={{ paddingRight: 0, paddingLeft: 0 }}>
+            <video className="video" autoPlay loop muted>
+                <source src={video} type="video/mp4"/>
+                Your browser does not support the video tag.
+            </video>
+            <Container className="overlay">
+                <Row className="h-100 justify-content-center align-items-center">
+                    <Col md={4} className="text-center">
+                        <h1>Main Title</h1>
+                        <p className="text-warning">This is a simple hero unit, a simple jumbotron-style component for calling extra attention to featured content or information.</p>
+                        <a className="btn btn-primary btn-lg" href="#" role="button">Learn more</a>
                     </Col>
                 </Row>
             </Container>
-        </section>
-    );
-  }
+        </Container>
+    </Fragment>
+  );
 }
 
 export default Main8;
